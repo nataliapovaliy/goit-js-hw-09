ed6df2fefb1d41a7330698dc9353a57f60fe2277
nataliapovaliy <natalia.bg1125@gmail.com>
2022-12-19 20:40:34 +0200

    update 01 ++

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,31 +1,24 @@
 const body = document.querySelector("body");
 const btnStart = document.querySelector("button[data-start]");
-const btnClose = document.querySelector("button[data-stop]");
+const btnStop = document.querySelector("button[data-stop]");
 let timerId = null;
 
-btnStart.addEventListener("click", () => {
+btnStart.addEventListener("click", onStart);
+btnStop.addEventListener("click", onStop);
+
+function onStart() {
+    btnStart.setAttribute("disabled", true);
+    btnStop.removeAttribute("disabled");
     timerId = setInterval(() => {
         body.style.backgroundColor = getRandomHexColor();
     }, 1000);
-});
+}
 
-btnClose.addEventListener("click", () => {
+function onStop() {
+    btnStop.setAttribute("disabled", true);
+    btnStart.removeAttribute("disabled");
     clearInterval(timerId);
-});
-
-
-
-
-
-// function onStart() {
-//     setInterval(() => {
-//         body.style.backgroundColor = getRandomHexColor();
-//     }, 1000);
-// }
-
-// function onClose() {
-//     clearInterval();
-// }
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
