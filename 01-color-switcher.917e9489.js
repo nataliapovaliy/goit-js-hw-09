fd0bf95e5465f4538c521765bb5302da639ca7bd
nataliapovaliy <105339076+nataliapovaliy@users.noreply.github.com>
2022-12-19 18:17:56 +0000

    Deploying to gh-pages from @ nataliapovaliy/goit-js-hw-09@281ed6a2b066a071e768cb67fb2decf83e1de9bd 🚀

--- 01-color-switcher.917e9489.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector([data-start]),o=document.querySelector([data-close]);e.addEventListener("click",(function(e){setTimeout((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),o.addEventListener("click",(function(t){}))}();
-//# sourceMappingURL=01-color-switcher.917e9489.js.map
