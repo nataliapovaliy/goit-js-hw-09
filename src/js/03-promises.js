ebf2cf3e7c840660c64a9a168da3f041164a50b4
nataliapovaliy <natalia.bg1125@gmail.com>
2022-12-22 00:11:23 +0200

    update 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,38 @@
 import Notiflix from 'notiflix';
 
-const formPromis = document.querySelector("form");
-const btn = formPromis.querySelector("button");
+const formPromis = document.querySelector('.form');
+
+formPromis.addEventListener("submit", onSubmit);
+
+function onSubmit (event) {
+  event.preventDefault();
+
+  let delayValue = Number(event.currentTarget.delay.value);
+  const stepValue = Number(event.currentTarget.step.value);
+  const amountValue = Number(event.currentTarget.amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayValue += stepValue;
+  }
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
